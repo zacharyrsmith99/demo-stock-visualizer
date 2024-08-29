8a13bdf6ec7c44f96818654dff4efb4326b58517
zacharyrsmith99 <zachary.r.smith99@gmail.com>
2024-08-29 09:39:01 -0400

    fix: aws credentials injection

--- start.sh
+++ start.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-
-source /opt/venv/bin/activate
-
-export PORT=$(aws secretsmanager get-secret-value --secret-id myapp/PORT --query SecretString --output text)
-export NODE_ENV=$(aws secretsmanager get-secret-value --secret-id myapp/NODE_ENV --query SecretString --output text)
-export LOG_LEVEL=$(aws secretsmanager get-secret-value --secret-id myapp/LOG_LEVEL --query SecretString --output text)
-export ENVIRONMENT=$(aws secretsmanager get-secret-value --secret-id myapp/ENVIRONMENT --query SecretString --output text)
-export TWELVEDATA_API_KEY=$(aws secretsmanager get-secret-value --secret-id myapp/TWELVEDATA_API_KEY --query SecretString --output text)
-export TWELVEDATA_WS_URL=$(aws secretsmanager get-secret-value --secret-id myapp/TWELVEDATA_WS_URL --query SecretString --output text)
-export COINBASE_WS_URL=$(aws secretsmanager get-secret-value --secret-id myapp/COINBASE_WS_URL --query SecretString --output text)
-export COINBASE_API_KEY=$(aws secretsmanager get-secret-value --secret-id myapp/COINBASE_API_KEY --query SecretString --output text)
-export COINBASE_API_PRIVATE_KEY=$(aws secretsmanager get-secret-value --secret-id myapp/COINBASE_API_PRIVATE_KEY --query SecretString --output text)
-
-envsubst < .env.tpl > .env
-
-deactivate
+export AWS_DEFAULT_REGION=us-east-1
+export PORT=$(aws secretsmanager get-secret-value --secret-id stockzrs-relay-secrets1 --query SecretString --output text | grep -o '"PORT":"[^"]*' |  grep -o '[^"]*$')
+export NODE_ENV=$(aws secretsmanager get-secret-value --secret-id stockzrs-relay-secrets1 --query SecretString --output text | grep -o '"NODE_ENV":"[^"]*' | grep -o '[^"]*$')
+export LOG_LEVEL=$(aws secretsmanager get-secret-value --secret-id stockzrs-relay-secrets1 --query SecretString --output text | grep -o '"LOG_LEVEL":"[^"]*' | grep -o '[^"]*$')
+export ENVIRONMENT=$(aws secretsmanager get-secret-value --secret-id stockzrs-relay-secrets1 --query SecretString --output text | grep -o '"ENVIRONMENT":"[^"]*' | grep -o '[^"]*$')
+export TWELVEDATA_API_KEY=$(aws secretsmanager get-secret-value --secret-id stockzrs-relay-secrets1 --query SecretString --output text | grep -o '"TWELVEDATA_API_KEY":"[^"]*' | grep -o '[^"]*$')
+export TWELVEDATA_WS_URL=$(aws secretsmanager get-secret-value --secret-id stockzrs-relay-secrets1 --query SecretString --output text | grep -o '"TWELVEDATA_WS_URL":"[^"]*' | grep -o '[^"]*$')
+export COINBASE_WS_URL=$(aws secretsmanager get-secret-value --secret-id stockzrs-relay-secrets1 --query SecretString --output text | grep -o '"COINBASE_WS_URL":"[^"]*' | grep -o '[^"]*$')
+export COINBASE_API_KEY=$(aws secretsmanager get-secret-value --secret-id stockzrs-relay-secrets1 --query SecretString --output text | grep -o '"COINBASE_API_KEY":"[^"]*' | grep -o '[^"]*$')
+export COINBASE_API_PRIVATE_KEY=$(aws secretsmanager get-secret-value --secret-id stockzrs-relay-secrets1 --query SecretString --output text | grep -o '"COINBASE_API_PRIVATE_KEY":"[^"]*' | grep -o '[^"]*$')
 
 node dist/index.js
